b17f4af583358debff5510b2b9c3cb38f45c3d9e
Christian Lauinger <christian.lauinger@gmail.com>
2025-05-14 18:33:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # glib-compile-schemas HeadsetControl\@lauinger-clan.de/schemas/
 
 cd SmartAutoMove\@lauinger-clan.de
-gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=../LICENSE
+gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=lib/common.js --extra-source=ui/prefs-adw.ui --extra-source=../LICENSE
 cd ..
 mv SmartAutoMove@lauinger-clan.de.shell-extension.zip SmartAutoMove@lauinger-clan.de.zip
 
