fdb77326803b36d1967b99b909901671503128d9
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-29 17:03:05 +0200

    Fixes extension installation and enabling
    Updates the install script to use the correct file name for installation and enable.
    This ensures that the extension is installed and enabled correctly.

--- install.sh
+++ install.sh
@@ -3,13 +3,22 @@
 # glib-compile-schemas HeadsetControl\@lauinger-clan.de/schemas/
 
 cd SmartAutoMoveNG\@lauinger-clan.de
-gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=./lib --extra-source=./ui/ --extra-source=./icons/ --extra-source=../LICENSE
+gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=./lib --extra-source=./ui/ --extra-source=./icons/ --extra-source=../LICENSE --force
 cd ..
-mv SmartAutoMoveNG@lauinger-clan.de.shell-extension.zip SmartAutoMoveNG@lauinger-clan.de.zip
 
-if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
-   echo "Extension zip created ..."
-else
-    gnome-extensions install SmartAutoMoveNG\@lauinger-clan.de.zip --force
-    gnome-extensions enable SmartAutoMoveNG\@lauinger-clan.de
-
+case "$1" in
+  zip|pack)
+    echo "Extension zip created ..."
+    ;;
+  install)
+    gnome-extensions install SmartAutoMoveNG\@lauinger-clan.de.shell-extension.zip --force
+    gnome-extensions enable SmartAutoMoveNG\@lauinger-clan.shell-extension.de
+    ;;
+  upload)
+    gnome-extensions upload SmartAutoMoveNG\@lauinger-clan.de.shell-extension.zip
+    ;;
+  *)
+    echo "Usage: $0 {zip|pack|install|upload}"
+    exit 1
+    ;;
+esac
