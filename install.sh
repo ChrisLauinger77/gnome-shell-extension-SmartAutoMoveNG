e92b5367b1ed8ee486391a87325e77dc86ca06e9
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-29 17:03:05 +0200

    Fixes extension installation and enabling
    Updates the install script to use the correct file name for installation and enable.
    This ensures that the extension is installed and enabled correctly.

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@
 cd SmartAutoMoveNG\@lauinger-clan.de
 gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=./lib --extra-source=./ui/ --extra-source=./icons/ --extra-source=../LICENSE
 cd ..
-mv SmartAutoMoveNG@lauinger-clan.de.shell-extension.zip SmartAutoMoveNG@lauinger-clan.de.zip
 
 if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
    echo "Extension zip created ..."
 else
-    gnome-extensions install SmartAutoMoveNG\@lauinger-clan.de.zip --force
-    gnome-extensions enable SmartAutoMoveNG\@lauinger-clan.de
+    gnome-extensions install SmartAutoMoveNG\@lauinger-clan.de.shell-extension.zip --force
+    gnome-extensions enable SmartAutoMoveNG\@lauinger-clan.shell-extension.de
 
